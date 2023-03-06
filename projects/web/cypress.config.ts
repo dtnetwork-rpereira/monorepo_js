8f670ac655d144d83d904c4469fc1c5937e76aa7
rpereiradt <r.pereira@dtnetwork.com.br>
2023-03-06 11:10:30 -0300

    removing video options

--- projects/web/cypress.config.ts
+++ projects/web/cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    screenshotOnRunFailure: false,
+    video: false,
     supportFile: false,
     specPattern: 'test/e2e/**/*.cy.{js,jsx,ts,tsx}',
   },
