114dca566cdc8dfafd2a9150b3f8e24685756c74
rpereiradt <r.pereira@dtnetwork.com.br>
2023-03-08 15:19:30 -0300

    Moving scripts again

--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -2,4 +2,9 @@
 
 printf "\n\033[4mProduction script provided by\033[0m \033[4;38;2;197;63;209mRCovery\033[0m\n\n"
 
-pm2 startOrReload ecosystem.config.js
+echo "Current directory: "
+pwd
+ls -la
+echo ""
+
+pm2 startOrReload ../ecosystem.config.js
