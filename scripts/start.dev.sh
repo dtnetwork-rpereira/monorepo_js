f289245db1829dda300f4bffb47bfc7f26d7d45d
rpereiradt <r.pereira@dtnetwork.com.br>
2023-03-08 14:01:13 -0300

    Using directory stack to execute script

--- scripts/start.dev.sh
+++ scripts/start.dev.sh
@@ -2,4 +2,6 @@
 
 printf "\n\033[4mDevelopment script provided by\033[0m \033[4;38;2;197;63;209mRCovery\033[0m\n\n"
 
-pm2 startOrReload ../dev.ecosystem.config.js
+pushd .. >/dev/null
+pm2 startOrReload dev.ecosystem.config.js
+popd >/dev/null
