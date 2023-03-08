15a463603f387620886f8992f04566403b45e128
rpereiradt <r.pereira@dtnetwork.com.br>
2023-03-08 17:10:04 -0300

    Fixing directorys

--- scripts/start.prod.sh
+++ scripts/start.prod.sh
@@ -2,9 +2,4 @@
 
 printf "\n\033[4mProduction script provided by\033[0m \033[4;38;2;197;63;209mRCovery\033[0m\n\n"
 
-echo "Current directory: "
-pwd
-ls -la
-echo ""
-
-pm2 startOrReload ../ecosystem.config.js
+pm2 startOrReload ecosystem.config.js
