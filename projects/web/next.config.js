e1038691af1809fa85f40d788d2e6ced8f048b44
rpereiradt <r.pereira@dtnetwork.com.br>
2023-03-10 11:00:22 -0300

    build: removing eslint from next build
    I'm already created a CI workflow that verify linting. The ESLint is a devDependency and not has to be in production environment.

--- projects/web/next.config.js
+++ projects/web/next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
         source: '/be/:path*',
-        destination: 'http://localhost:3001/:path*'
-      }
-    ]
-  }
-}
+        destination: 'http://localhost:3001/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
